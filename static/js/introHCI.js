8f5bb6d6f042af825d207c30abc11bc7e27165f3
nick <nick.thenebels@gmail.com>
2020-01-23 11:28:05 -0800

    added more js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions\. Too many questions is the Chan disease\. The best way is just to observe the noise of the world\. The answer to your questions\? Ask your own heart\.");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
+    });
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+	$(this).css("background-color", "#7fff00");
+	
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.fadeOut();
+    }
 }
